7630eed8551759c504b28ba2b86c5eeccfca954e
Eswari1607 <eswarikamala1607@gmail.com>
2025-10-28 12:07:21 +0000

    Add generated Playwright test

---
+++ pages/TestscriptforaddnewpersonPage.ts
@@ -0,0 +1,34 @@
+import { Page, Locator } from '@playwright/test';
+import locators from "../locators/test-script-for-add-new-person.ts";
+
+class Testscriptforaddnewpersonpage {
+  page: Page;
+  userName: Locator;
+  password: Locator;
+  signIn: Locator;
+  enterPasscode: Locator;
+  verify: Locator;
+  navigator: Locator;
+  myTeam: Locator;
+  newPerson: Locator;
+  newPersonDashboard: Locator;
+  view: Locator;
+  viewFormatQueryByExampleFreezeFreezeWrapWrapPersonPersonNumberLegalEmployerProposedStartDa: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.userName = page.locator(locators.userName);
+    this.password = page.locator(locators.password);
+    this.signIn = page.locator(locators.signIn);
+    this.enterPasscode = page.locator(locators.enterPasscode);
+    this.verify = page.locator(locators.verify);
+    this.navigator = page.locator(locators.navigator);
+    this.myTeam = page.locator(locators.myTeam);
+    this.newPerson = page.locator(locators.newPerson);
+    this.newPersonDashboard = page.locator(locators.newPersonDashboard);
+    this.view = page.locator(locators.view);
+    this.viewFormatQueryByExampleFreezeFreezeWrapWrapPersonPersonNumberLegalEmployerProposedStartDa = page.locator(locators.viewFormatQueryByExampleFreezeFreezeWrapWrapPersonPersonNumberLegalEmployerProposedStartDa);
+  }
+}
+
+export default Testscriptforaddnewpersonpage;
